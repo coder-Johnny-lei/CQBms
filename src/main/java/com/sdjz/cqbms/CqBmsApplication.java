b6d5fa7b14ab10b57bef9769f0f64963d6972b8d
jinlei <jinlei@hiwin-gsv.com>
2022-05-18 11:25:26 +0800

    Initial commit

---
+++ src/main/java/com/sdjz/cqbms/CqBmsApplication.java
@@ -0,0 +1,13 @@
+package com.sdjz.cqbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqBmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqBmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdjz/cqbms/CqBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjz.cqbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqBmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
